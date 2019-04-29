3e526eca8cec012b9faa2a1f9a01bd7e6eb84990
Iana-Neri22 <iananeri@outlook.com>
2019-04-29 16:34:23 -0300

    initial tests on checklength

--- test/cpf-validator.spec.js
+++ test/cpf-validator.spec.js
@@ -2,3 +2,10 @@ let chai = require('chai');
 let expect = chai.expect;
 let CPF = require('../lib/cpf-validator');
 
+  describe('cpf lib', () => {
+    describe('checkLength', () => {
+      it('Return false to 123456', () => {
+        expect(CPF.checkLength("123456")).to.equal(false);
+      });
+    });
+  });
