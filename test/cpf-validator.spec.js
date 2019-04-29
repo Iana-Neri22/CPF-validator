576fe991554cbf5feae66f14f54c1f3ac6f3a6cc
Iana-Neri22 <iananeri@outlook.com>
2019-04-29 18:59:51 -0300

    multiplying in ES6

--- test/cpf-validator.spec.js
+++ test/cpf-validator.spec.js
@@ -3,9 +3,9 @@ let expect = chai.expect;
 let CPF = require('../lib/cpf-validator');
 
   describe('cpf lib', () => {
-    describe('checkLength', () => {
+    describe('cpfValidator', () => {
       it('Return false to 123456', () => {
-        expect(CPF.checkLength("123456")).to.equal(false);
+        expect(CPF.cpfValidator("123456")).to.equal(false);
       });
     });
   });
